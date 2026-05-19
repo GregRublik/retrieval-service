61d5d8087eee571ea15a843f7afe83925405a9bd
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-05-19 15:21:23 +0300

    commenting r.raise_for_status from fetch_one

--- src/services/fetcher.py
+++ src/services/fetcher.py
@@ -11,7 +11,7 @@ class FetchService:
     async def fetch_one(self, link: ResultWebSearch) -> RawPage:
         """Fetch (получить) данные с web страницы"""
         r = await self.session.get(link.url)
-        r.raise_for_status()
+        # r.raise_for_status()
         html = await r.text()
         return RawPage(url=link.url, html=html, score=link.score, title=link.content)
 
